09c1dbd040f2caf846f1413d91b545cbd2e8227d
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2022-07-15 13:04:23 +0800

    add

--- repair.cpp
+++ repair.cpp
@@ -2,7 +2,10 @@
 #include<iostream>
 #include <TlHelp32.h>
 #include<string>
+#include<cmath>
 #include"signatures.hpp"
+
+#define PI 3.14159265358979323846
 
 /* This fixed buffer was coded by SexOffenderSally, a long time friend! Give him a <3 on discord
 Make sure character set is 'Multi-Byte' in project settings! And game must be windowed fullscreen.
@@ -10,7 +13,8 @@ Updated offsets: https://github.com/frk1/hazedumper/blob/master/csgo.cs     */
 
 using namespace offsets;
 
-uintptr_t moduleBase;
+uintptr_t clientBase;
+uintptr_t engineBase;
 HANDLE TargetProcess;
 HPEN BoxPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
 RECT WBounds;
@@ -22,6 +26,10 @@ template<typename T> T RPM(SIZE_T address) {
 	return buffer;
 }
 
+template <typename var>
+bool WPM(DWORD address, var value) {
+	return WriteProcessMemory(TargetProcess, (LPVOID)address, &value, sizeof(var), NULL);
+}
 
 uintptr_t GetModuleBaseAddress(DWORD tPID, const std::string_view moduleName) {
 	HANDLE hmodule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, tPID);
@@ -93,6 +101,35 @@ struct Vector3 WorldToScreen(const struct Vector3 pos, struct view_matrix_t matr
 	return out;
 }
 
+//空间向量夹角的计算公式是cosθ＝a* b / (| a | *| b | )
+float angleOfTwoVector3(Vector3 a, Vector3 b) {
+	//TODO
+	float ab = a.x * b.x + a.y * b.y + a.z * b.z;
+	float ab2 = sqrt(pow(a.x, 2) + pow(a.y, 2) + pow(a.z, 2))
+		* sqrt(pow(b.x, 2) + pow(b.y, 2) + pow(b.z, 2));
+
+	return acos(ab / ab2) * 180 / PI;
+}
+
+uintptr_t get_local_player() {
+	DWORD clientState = RPM<DWORD>(engineBase + offsets::dwClientState);
+	DWORD player_id = RPM<DWORD>(clientState + offsets::dwClientState_GetLocalPlayer);
+	return  RPM<DWORD>(clientBase + offsets::dwEntityList + player_id * 0x10);
+}
+
+Vector3 getViewPosition(uintptr_t playerEnt) {
+	Vector3 playerPos = RPM<Vector3>(playerEnt + offsets::m_vecOrigin);
+	Vector3 eyeOffset = RPM<Vector3>(playerEnt + offsets::m_vecViewOffset);
+
+	return Vector3(playerPos.x + eyeOffset.x,
+		playerPos.y + eyeOffset.y,
+		playerPos.z + eyeOffset.z);
+}
+
+Vector3 getPosition(uintptr_t playerEnt) {
+	return RPM<Vector3>((playerEnt + offsets::m_vecOrigin));
+}
+
 
 void DrawPlayer(HDC hdc, Vector3 foot, Vector3 head, int health, uintptr_t pEnt) {
 	float height = head.y - foot.y;
@@ -141,6 +178,91 @@ void DrawPlayer(HDC hdc, Vector3 foot, Vector3 head) {
 	Rectangle(hdc, foot.x - (width / 2), foot.y, head.x + (width / 2), head.y);
 }
 
+void DrawPlayerScreenDistanceToCrosshair(HDC hdc, Vector3 foot, Vector3 head, uintptr_t player) {
+	auto raw_color = GetTextColor(hdc);
+	SetTextColor(hdc, RGB(0, 255, 0));
+
+	Vector3 crosshair = Vector3(2560/ 2, 1440/ 2,  0);
+
+	//敌人位置
+	Vector3 player_position = getPosition(player);
+
+	Vector3 player_position_screen = WorldToScreen(player_position, RPM<view_matrix_t>(clientBase + dwViewMatrix));
+
+	float crosshair_distance_to_player = sqrt(pow(player_position_screen.x - crosshair.x, 2) + pow(player_position_screen.y - crosshair.y, 2));
+	
+	std::string crosshair_distance_to_player_str = std::string("distance_to_crosshair: ") + std::to_string(crosshair_distance_to_player);
+
+	TextOutA(hdc, foot.x, foot.y, crosshair_distance_to_player_str.c_str(), crosshair_distance_to_player_str.size());
+
+
+
+	SetTextColor(hdc, raw_color);
+}
+/*
+void DrawPlayerAngleToLocalPlayer(HDC hdc, Vector3 foot, Vector3 head, uintptr_t player) {
+	auto raw_color = GetTextColor(hdc);
+	SetTextColor(hdc, RGB(0, 255, 0));
+
+
+
+	float angle = 0;
+
+	//敌人眼球位置
+	Vector3 player_position = getPosition(player);
+	Vector3 local_player_view_position = getViewPosition(get_local_player());
+	local_player_view_position.z;
+
+	DWORD clst = RPM<DWORD>(engineBase + offsets::dwClientState);
+	//Vector3 view_angles = Vector3(0, 90, 0);
+	//WPM<Vector3>(clst + offsets::dwClientState_ViewAngles, view_angles);
+
+	Vector3 view_angles = RPM<Vector3>(clst + offsets::dwClientState_ViewAngles);
+
+
+	//pitch_angle=0时玩家朝向x轴正方向，左肩朝向y轴正方向, pitch_angle增大时，玩家向y轴旋转，即向左旋转
+	//pitch_angle=0 => direction = (1, 0, 0)
+
+	//x = cos(yaw) * cos(pitch)
+	//y = sin(yaw) * cos(pitch)
+	//z = sin(pitch)
+	Vector3 dirction_of_view = Vector3(cos(view_angles.y * PI / 180) * cos(view_angles.x * PI / 180),
+										sin(view_angles.y * PI / 180) * cos(view_angles.x * PI / 180),
+										sin(view_angles.x * PI / 180));//TODO
+	Vector3 direction_to_enemy = Vector3(player_position.x - local_player_view_position.x,
+										player_position.y - local_player_view_position.y,
+										player_position.z - local_player_view_position.z);//TODO
+
+	
+	std::cout << "direction to enemy:" << direction_to_enemy.x << ", " <<
+		direction_to_enemy.y << ", " <<
+		direction_to_enemy.z << std::endl;
+
+	std::cout << "dirction_of_view:" << dirction_of_view.x << ", " <<
+		dirction_of_view.y << ", " <<
+		dirction_of_view.z << std::endl;
+	std::cout << std::endl;
+
+
+	//计算两方向向量夹角
+	//空间向量夹角的计算公式是cosθ＝a* b / (| a | *| b | )
+	angle = angleOfTwoVector3(dirction_of_view, direction_to_enemy);
+
+
+	std::string angle_str = ("angle: ");
+	std::string yaw_str = ("yaw(y): ") + std::to_string(view_angles.y);
+	std::string pitch = ("pitch(x): ") + std::to_string(view_angles.x);
+
+	angle_str += std::to_string(angle);
+	TextOutA(hdc, foot.x , foot.y, angle_str.c_str(), angle_str.size());
+
+	TextOutA(hdc, 200, 300, yaw_str.c_str(), yaw_str.size());
+	TextOutA(hdc, 200, 330, pitch.c_str(), pitch.size());
+
+
+	SetTextColor(hdc, raw_color);
+}*/
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
 	case WM_PAINT: {
@@ -159,11 +281,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		FillRect(Memhdc, &WBounds, WHITE_BRUSH);
 
 
-		view_matrix_t vm = RPM<view_matrix_t>(moduleBase + dwViewMatrix);
-		int localteam = RPM<int>(RPM<DWORD>(moduleBase + dwEntityList) + m_iTeamNum);
+		view_matrix_t vm = RPM<view_matrix_t>(clientBase + dwViewMatrix);
+		int localteam = RPM<int>(RPM<DWORD>(clientBase + dwEntityList) + m_iTeamNum);
 
 		for (int i = 1; i < 64; i++) {
-			uintptr_t pEnt = RPM<DWORD>(moduleBase + dwEntityList + (i * 0x10));
+			uintptr_t pEnt = RPM<DWORD>(clientBase + dwEntityList + (i * 0x10));
 			int team = RPM<int>(pEnt + m_iTeamNum);
 
 			if (team != localteam) {
@@ -178,6 +300,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
 				if (screenpos.z >= 0.01f && health > 0 && health < 101 && !RPM<bool>(pEnt + m_bDormant)) {
 					DrawPlayer(Memhdc, screenpos, screenhead, health, pEnt);
+					DrawPlayerScreenDistanceToCrosshair(Memhdc, screenpos, screenhead, pEnt);
 				}
 			}
 		}
@@ -216,7 +339,9 @@ int main() {
 	GetClientRect(GameHWND, &WBounds);
 	auto pID = GetProcId(std::string_view("csgo.exe"));
 	TargetProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, pID);
-	moduleBase = GetModuleBaseAddress(pID, std::string_view("client.dll"));
+	clientBase = GetModuleBaseAddress(pID, std::string_view("client.dll"));
+	engineBase = GetModuleBaseAddress(pID, std::string_view("engine.dll"));
+
 
 	WNDCLASSEX WClass;
 	MSG Msg;
